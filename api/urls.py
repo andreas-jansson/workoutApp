e71f332642834e0bd65f1fc17a78ec728b89fa05
andreas-jansson <andreas.jansson@protonmail.com>
2021-09-17 20:29:12 +0200

    new branch with changes

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, SessionExistView
 
 urlpatterns = [
     path('', UserView.as_view()),
+    path('session-exist', SessionExistView.as_view()),
 ]
