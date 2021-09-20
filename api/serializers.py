9c92cf8f60e1aa0b1fb446a9de95b9567b5dc4f4
viklih181 <viklih181@studentmail.oru.se>
2021-09-20 16:13:15 +0200

    added serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,44 @@
 from rest_framework import serializers
-from .models import User
+from .models import Role, ExerciseType, Exercise, Workout, \
+    User, scheduledWorkout, Log 
 
 class UserSerializer(serializers.Serializer):
     class Meta:
         model = User
         fields = ('id', 'fname', 'lname', 'email', 'pwhash', 'salt', 'roleid', 'created')
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = '__all__'
+
+
+class ExerciseTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseType
+        fields = '__all__'
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = '__all__'
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = '__all__'
+
+
+class scheduledWorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = scheduledWorkout
+        fields = '__all__'
+
+
+class LogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Log
+        fields = '__all__'
