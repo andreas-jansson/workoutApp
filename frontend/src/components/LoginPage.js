211e29c0b56b3111c71cb70cf9bb4b421bb9db3e
Maghangordan <47462239+Maghangordan@users.noreply.github.com>
2021-09-21 17:13:19 +0200

    Login page beautified

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,38 +1,107 @@
-import React, {Component, useEffect} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
 
+export default class LoginPage extends Component {
 
+  static defaultProps = {
+  };
 
-export default class LoginPage extends Component{
-
-    static defaultProps = {
-    };
-
-    constructor(props){
-        super(props);
-
-        this.state={
-            sessionActive: false,
-        };
-    }
-
-    LoginPage(){
-        return(
-            <div className="db-container">
-                <h1>Login</h1>
-                <label>
-                    AWOOOOOOOOOO
-                </label>
-            </div>
-        );
-    }
+  constructor(props) {
     
+    super(props);
+    //this.state = { sessionActive: false };
+    this.state = { email: "", password: "" };
 
+    this.OnSignIn = this.OnSignIn.bind(this);
+    this.EmailChange = this.EmailChange.bind(this);
+    this.PasswordChange = this.PasswordChange.bind(this);
+  }
 
-    render(){
-        return(
-             this.LoginPage()
-         );
+  EmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  PasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  OnSignIn(event) {
+    if (this.state.email == "test" && this.state.password == "hej") {
+      alert("Hello World");
+    } else {
+      alert(this.state.email);
     }
+    event.preventDefault();
+  }
 
+  RenderHeader() {
+    return (
+      <div className="header-container">
+        <div className="header-btn-container">
+          <Link to="/login">
+            <div className="header-btn-btn1">Pricing</div>
+          </Link>
+          <Link to="/login">
+            <div className="header-btn-btn2">About</div>
+          </Link>
+          <Link to="">
+            <div className="header-btn-btn3">Home</div>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  LoginPage() {
+    return (
+      <div className="sp-section lp-container">
+        {this.RenderHeader()}
+        <div className="box-centering">
+        <div className="login-box box-centering">
+          <div align="center" className="login-padding">
+            Email
+          </div>
+          <form>
+            <label>
+              <input
+                className="login-text"
+                type="text"
+                email={this.state.email}
+                onChange={this.EmailChange}
+                maxLength="50"
+              />
+            </label>
+            <div align="center" className="login-padding">
+              Password
+            </div>
+            <label>
+              <input
+                className="login-text"
+                type="password"
+                password={this.state.password}
+                onChange={this.PasswordChange}
+                maxLength="30"
+              />
+            </label>
+            <div align="center">
+              <p>
+                <button
+                  className="myButton"
+                  variant="contained"
+                  onClick={this.OnSignIn}
+                >
+                  Sign In
+                </button>
+              </p>
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (this.LoginPage());
+  }
 }
