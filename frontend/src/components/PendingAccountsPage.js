74c76a322adc44c64a77d6681fcd8166f7b2a29f
=dannelar100 <=dannelar100@gmail.com>
2021-10-01 13:27:33 +0200

    Fixed basic styling of PendingAccount, stole CSS from createAccountPage

--- frontend/src/components/PendingAccountsPage.js
+++ frontend/src/components/PendingAccountsPage.js
@@ -2,6 +2,12 @@ import React, { Component, useState, useEffect } from "react";
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
 import CheckIcon from '@material-ui/icons/Check'
+import '../../static/css/register-coach.css';
+import {green, red} from '@material-ui/core/colors'
+import { createTheme } from "@material-ui/core/styles";
+import { MuiThemeProvider } from "@material-ui/core";
+
+const my_theme = createTheme({palette: {primary: green, secondary: red}})
 
 export default class PendingAccountsPage extends Component{
   constructor(props){
@@ -71,15 +77,20 @@ export default class PendingAccountsPage extends Component{
       return <div>Loading ... </div>;
     } else {
       return (
+        <div className="cap-container">
+        <div className="cap-section">
         <ul>
           {users.map(item => (
             <div className = '' key={item.id}>
               <div>{item.fname} {item.lname} | {item.email}
-              <IconButton onClick={this.approveUser} value={item.id} aria-label="check" size="large" color = "success"><CheckIcon /></IconButton>
-              <IconButton onClick={this.denyUser} value={item.id} aria-label="delete" size="large" color = "secondary"><DeleteIcon /></IconButton></div>
+              <MuiThemeProvider theme={my_theme}><IconButton onClick={this.approveUser} value={item.id} aria-label="check" size="large" color="primary"><CheckIcon /></IconButton></MuiThemeProvider>
+              <MuiThemeProvider theme={my_theme}><IconButton onClick={this.denyUser} value={item.id} aria-label="delete" size="large" color="secondary"><DeleteIcon /></IconButton></MuiThemeProvider>
+              </div>
             </div>
           ))}
         </ul>
+        </div>
+        </div>
       );
     }
   }
