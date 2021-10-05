daff2f844cabed0410eaff848afffefb2bae992f
TheFakeMZ <alipoppin101@gmail.com>
2021-10-05 20:13:28 +0200

    Updated Validation for Signup form (last & first name)

--- frontend/src/components/ValidateRegistration.js
+++ frontend/src/components/ValidateRegistration.js
@@ -5,24 +5,54 @@ const validateSignUpForm = payload => {
   let message = "";
   let isFormValid = true;
 
+  let nameCheck = /^[a-zA-Z]+$/
+  
+
+    //   First name 
   if (
     !payload ||
     typeof payload.fname !== "string" ||
-    payload.fname.trim().length === 0
+    payload.fname.trim().length === 0 ||
+    (payload.fname.trim().length > 0 && payload.fname.trim().length < 3) ||
+    !nameCheck.test(payload.fname)
   ) {
-    isFormValid = false;
-    errors.fname = "Please provide a first name.";
+    if (!nameCheck.test(payload.fname)) {
+      isFormValid = false;
+      errors.fname = "Invalid characters.";
+    }
+    if (payload.fname.trim().length === 0) {
+      isFormValid = false;
+      errors.fname = "Please provide a first name.";
+    }
+    if (payload.fname.trim().length > 0 && payload.fname.trim().length < 3) {
+      isFormValid = false;
+      errors.fname = "First name must contain at least 3 characters.";
+    }
   }
 
+  //   Last name 
   if (
     !payload ||
     typeof payload.lname !== "string" ||
-    payload.lname.trim().length === 0
+    payload.lname.trim().length === 0 ||
+    (payload.lname.trim().length > 0 && payload.lname.trim().length < 3) ||
+    !nameCheck.test(payload.lname)
   ) {
-    isFormValid = false;
-    errors.lname = "Please provide a last name.";
+    if (!nameCheck.test(payload.lname)) {
+      isFormValid = false;
+      errors.lname = "Invalid characters.";
+    }
+    if (payload.lname.trim().length === 0) {
+      isFormValid = false;
+      errors.lname = "Please provide a first name.";
+    }
+    if (payload.lname.trim().length > 0 && payload.lname.trim().length < 3) {
+      isFormValid = false;
+      errors.lname = "First name must contain at least 3 characters.";
+    }
   }
 
+    //   Email
   if (
     !payload ||
     typeof payload.email !== "string" ||
@@ -32,20 +62,23 @@ const validateSignUpForm = payload => {
     errors.email = "Please provide a correct email address.";
   }
 
+    //   Password
   if (
     !payload ||
     typeof payload.password !== "string" ||
     payload.password.trim().length < 8
   ) {
     isFormValid = false;
-    errors.password = "Password must have at least 8 characters.";
+    errors.password = "Password must contain at least 8 characters.";
   }
 
+    //   Confirm Password
   if (!payload || payload.confirmed_password !== payload.password) {
     isFormValid = false;
     errors.confirmed_password = "Password confirmation doesn't match.";
   }
 
+    //   Validity check
   if (!isFormValid) {
     message = "Check the form for errors.";
   }
