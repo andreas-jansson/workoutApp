bf53dd6d4b6d4d85e45de522cc992bde94df6be7
TheFakeMZ <alipoppin101@gmail.com>
2021-10-07 05:29:30 +0200

    Updated Table functionality (view connection still iffy)

--- frontend/src/components/ManageAccountsPage.js
+++ frontend/src/components/ManageAccountsPage.js
@@ -5,103 +5,161 @@ import "../../static/css/manage-account-table.css";
 function ManageAccountsPage() {
   const url = "api/get-user";
   const [data, setData] = useState([]);
-  useEffect(() => {
-    getUsers();
-  }, []);
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  const columns = [
+    {
+      title: "User ID",
+      field: "id",
+      editable: "never",
+      filtering: false,
+    },
+    {
+      title: "First name",
+      field: "fname",
+      filtering: false,
+      validate: rowData => rowData.fname.length < 3 ? { isValid: false, helperText: 'Forename must be longer than 3 Characters' } : true,
+    },
+    {
+      title: "Last name",
+      field: "lname",
+      filtering: false,
+      validate: rowData => rowData.lname.length < 3 ? { isValid: false, helperText: 'Surname must be longer than 3 Characters' } : true,
+    },
+    {
+      title: "Email Address",
+      field: "email",
+      filtering: false,
+      validate: rowData => rowData.email === '' ? { isValid: false, helperText: 'Email cannot be empty' } : true,
+    },
+    {
+      title: "Role ID",
+      field: "roleid",
+      lookup: { 1: "Pending", 2: "Client", 3: "Coach", 4: "Admin" },
+    },
+    {
+      title: "Time of Creation",
+      field: "created",
+      editable: "never",
+      filtering: false,
+    },
+  ];
 
   const getUsers = () => {
     fetch(url)
       .then((resp) => resp.json())
       .then((resp) => setData(resp));
   };
-  
-  const columns = [
-    {
-      title: "User ID",
-      field: "id",
-    },
-    {
-      title: "First name",
-      field: "fname",
-    },
-    {
-      title: "Last name",
-      field: "lname",
-    },
-    {
-      title: "Email Address",
-      field: "email",
-    },
-    {
-      title: "Role ID",
-      field: "roleid",
-    }
-  ];
-  return (
-    <div> 
-      <div className='mactable-signup-text'>
-        <h1 align='center'>Account Management</h1>
-      </div>
 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <div className="mactable-signup-text"></div>
       <div className="mactable-container">
-      <br/>    <br/>
-        <MaterialTable 
-         style='StyledTableCell'
-          title="User Details"
+        <br /> <br />
+        <MaterialTable
+          style={{
+            fontSize: 20,
+            textAlign: "center",
+            backgroundColor: "white",
+          }}
+          cellEditable={{
+            cellStyle: {},
+            onCellEditApproved: (newValue, oldValue, rowData, columnDef) => {
+              return new Promise((resolve, reject) => {
+                fetch(url, {
+                  method: "PUT",
+                  headers: {
+                    "Content-type": "application/json",
+                  },
+                  body: newData,
+                })
+                  .then((resp) => resp.json())
+                  .then((resp) => {
+                    getUsers();
+                    resolve();
+                  });
+              });
+            },
+          }}
+          title={
+            <div  className="mactable-signup-text">
+            <h2>
+              Account Management Table
+            </h2>
+              </div>
+          }
+          onRowClick={(evt, selectedRow) =>
+            setSelectedRow(selectedRow.tableData.id)
+          }
+          options={{
+            headerStyle: {
+              backgroundColor: "orange",
+              color: "#FFF",
+              fontWeight: "bolder",
+              fontSize: 20,
+              fontStyle:'roboto'
+            },
+            rowStyle: (rowData) => ({
+              backgroundColor:
+                selectedRow === rowData.tableData.id ? "#484849" : "#ededef",
+              color: 
+                selectedRow === rowData.tableData.id ? "#FFF" : "black",
+            }),
+            searchFieldStyle: {
+              fontWeight: "bolder",
+            },
+            exportButton: {
+              csv: true,
+              pdf: false
+            },
+            actionsColumnIndex: -1,
+            addRowPosition: "first",
+            filtering: true,
+          }}
+          localization={{
+            toolbar: {
+              exportCSVName: "Export some Excel format",
+              exportPDFName: "Export as pdf!!"
+            }
+          }}
           columns={columns}
           data={data}
-          options={{ actionsColumnIndex: -1, addRowPosition: "first" }}
           editable={{
-            onRowAdd: (newData) =>
-            new Promise((resolve, reject) => {
-              //Backend call
-              fetch(url, {
-                method: "POST",
-                headers: {
-                  "Content-type": "application/json",
-                },
-                body: JSON.stringify(newData),
-              })
-              .then((resp) => resp.json())
-              .then((resp) => {
-                getUsers();
-                resolve();
-              });
-            }),
             onRowUpdate: (newData, oldData) =>
-            new Promise((resolve, reject) => {
-              //Backend call
-              fetch(url + "/" + oldData.id, {
-                method: "PUT",
-                headers: {
-                  "Content-type": "application/json",
-                },
-                body: JSON.stringify(newData),
-              })
-              .then((resp) => resp.json())
-              .then((resp) => {
-                getUsers();
-                resolve();
-              });
-            }),
+              new Promise((resolve, reject) => {
+                fetch(url + "/" + oldData.id, {
+                  method: "PUT",
+                  headers: {
+                    "Content-type": "application/json",
+                  },
+                  body: newData,
+                })
+                  .then((resp) => resp.json())
+                  .then((resp) => {
+                    getUsers();
+                    resolve();
+                  });
+              }),
             onRowDelete: (oldData) =>
-            new Promise((resolve, reject) => {
-              //Backend call
-              fetch(url + "/" + oldData.id, {
-                method: "DELETE",
-                headers: {
-                  "Content-type": "application/json",
-                },
-              })
-              .then((resp) => resp.json())
-              .then((resp) => {
-                getUsers();
-                resolve();
-              });
-            }),
+              new Promise((resolve, reject) => {
+                fetch(url + "/" + oldData.id, {
+                  method: "DELETE",
+                  headers: {
+                    "Content-type": "application/json",
+                  },
+                })
+                  .then((resp) => resp.json())
+                  .then((resp) => {
+                    getUsers();
+                    resolve();
+                  });
+              }),
           }}
-          />
-
+        />
       </div>
     </div>
   );
