180da6f8a55d41d6483d3103430fe07d5b2a8ea8
v-lnd <90709747+v-lnd@users.noreply.github.com>
2021-10-08 18:03:07 +0200

    Update SettingsPage.js
    More work on this.

--- frontend/src/components/SettingsPage.js
+++ frontend/src/components/SettingsPage.js
@@ -3,40 +3,87 @@ import Switch from '@material-ui/core/Switch';
 import { alpha, styled } from '@material-ui/core/styles';
 
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
-
+/*Page to put all user settings*/
 /*Switch in order to make yourself searchable or not searchable for other clients within the social media system*/
 
+
+/*TODO: Fixa så att switchen är av eller på vid load baserat på om
+isVisible for users är true eller false i databasen*/
 const OrangeSwitch = styled(Switch)(({ theme }) => ({
     '& .MuiSwitch-switchBase.Mui-checked': {
-      color: "#e66a04",
-      '&:hover': {
-        backgroundColor: alpha("#e66a04", theme.palette.action.hoverOpacity),
-      },
+        color: "#e66a04",
+        '&:hover': {
+            backgroundColor: alpha("#e66a04", theme.palette.action.hoverOpacity),
+        },
     },
     '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
-      backgroundColor: "#e66a04",
+        backgroundColor: "#e66a04",
     },
     '& .MuiSwitch-switchBase + .MuiSwitch-track': {
         backgroundColor: "white",
-      },
+    },
     '& .MuiSwitch-switchBase': {
         color: "lightgrey"
     },
-  }));
+}));
 
 export default class SettingsPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-         /*Write state stuff here please yes*/   
+            isVisible: false,
+            sessionActive: false,
         }
     }
 
-render() {
-    return (
-        <div>
-            <OrangeSwitch {...label}/>
-      </div>
-    );
-  };
+    sendDetailsToServer = () => {
+        let isVisible = this.state.isVisible;
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            isVisible,
+          }),
+        };
+        console.log(requestOptions.body);
+        fetch("/api/change-user-visibilty", requestOptions).then((response) => {
+          if (response.ok) {
+            console.log("sendDetailsToServer() successful")
+          } else {
+            console.log("sendDetailsToServer() not OK")
+          }
+        });
+      };
+
+
+    handleChange = () => {
+        this.setState({
+         isVisible: !this.state.isVisible,
+        })
+        this.sendDetailsToServer();
+      }
+
+    render() {
+        const mystyle = {
+            color: "white",
+            paddingLeft: "10px",
+            fontFamily: "Arial"
+          };
+        return (
+            <div>
+                <div>
+                <p style={mystyle}>
+                    Push below button in order
+                    <br/>
+                    to make yourself searchable
+                    <br/>
+                    to other gym members.
+                </p>
+                </div>
+                <div>
+                    <OrangeSwitch onChange={() => {this.handleChange();}}{...label} />
+                </div>
+            </div>
+        );
+    };
 }
